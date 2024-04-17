dd97b5275417564b1c4b74270b7a04bdeaa650c1
nicolaishestakov <nicolai.shestakov@gmail.com>
2024-04-18 00:53:31 +0200

    terraform metrics-parquet-view table

--- project/terraform/main.tf
+++ project/terraform/main.tf
@@ -59,3 +59,25 @@ EOF
 
   depends_on = [google_bigquery_dataset.metrics_dataset]
 }
+
+resource "google_bigquery_table" "external_table" {
+  dataset_id = google_bigquery_dataset.metrics_dataset.dataset_id
+  table_id   = "metrics-parquet-view"
+  
+  schema = <<EOF
+[
+  {"name": "commit", "type": "STRING"},
+  {"name": "filepath", "type": "STRING"},
+  {"name": "area", "type": "STRING"},
+  {"name": "std_code_lines_code", "type": "FLOAT"},
+  {"name": "std_code_complexity_cyclomatic", "type": "FLOAT"}
+]
+EOF
+
+  depends_on = [google_bigquery_dataset.metrics_dataset]
+  external_data_configuration {
+    autodetect    = false
+    source_format = "PARQUET"
+    source_uris   = ["gs://${var.gcs_bucket_name}/metrics_data/*.parquet"]
+  }
+}
